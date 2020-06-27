1cb834b798353d7e752989c660b9334f09278894
ivanmartos <ivan.martos@crowdhouse.ch>
2020-06-27 10:20:22 +0200

    lambda for uploading timesheet

--- internal/timesheetUploader/timeTrackingService.go
+++ internal/timesheetUploader/timeTrackingService.go
@@ -1,47 +1,26 @@
 package timesheetUploader
 
 import (
-	"fmt"
-	"log"
-	"time"
+	"github.com/ivanmartos/bamboo-tracker/internal/model"
 )
 
-type TimeTrackingService struct {
-	api           BambooApi
-	sesRepository SesRepository
+type TimeTrackingServiceImpl struct {
+	api BambooApi
 }
 
-func InitTimeTrackingService(api BambooApi, sesRepository SesRepository) TimeTrackingService {
-	return TimeTrackingService{
-		api:           api,
-		sesRepository: sesRepository,
+func InitTimeTrackingService(api BambooApi) TimeTrackingService {
+	return TimeTrackingServiceImpl{
+		api: api,
 	}
 }
 
-func (s TimeTrackingService) SendCurrentTimeTrackEmail() {
+func (s TimeTrackingServiceImpl) GetCurrentTimeTracking() model.TimeTracking {
 	s.api.LogIn(getEnvVariable("BAMBOO_USERNAME"), getEnvVariable("BAMBOO_PASSWORD"))
 	timeTracking := s.api.GetHomeContent()
+	return timeTracking
+}
 
-	subject := fmt.Sprintf("BambooHr timesheet report for %s %s", time.Now().Weekday().String(), time.Now().Format("02.01.2006"))
-
-	htmlTemplate := `
-<html>
-<body>
-<h3>%s</h3>
-<p>Logged time for today - %f</p>
-<p>Logged time for the week - %f</p>
-</body>
-</html>
-`
-	html := fmt.Sprintf(htmlTemplate, subject, timeTracking.DailyTotal, timeTracking.WeeklyTotal)
-
-	log.Println(html)
-
-	s.sesRepository.SendEmail(
-		getEnvVariable("DAILY_TIME_TRACKING_SENDER_EMAIL"),
-		getEnvVariable("DAILY_TIME_TRACKING_RECIPIENT_EMAIL"),
-		html,
-		subject)
-
-	log.Println(timeTracking.DailyTotal)
+func (s TimeTrackingServiceImpl) UploadTimesheetEntries(timesheetEntries []model.TimesheetEntry) {
+	session := s.api.LogIn(getEnvVariable("BAMBOO_USERNAME"), getEnvVariable("BAMBOO_PASSWORD"))
+	s.api.AddTimesheetRecord(session, timesheetEntries)
 }
--- internal/timesheetUploader/timeTrackingService_test.go
+++ internal/timesheetUploader/timeTrackingService_test.go
@@ -4,63 +4,96 @@ import (
 	"github.com/ivanmartos/bamboo-tracker/internal/mocks"
 	"github.com/ivanmartos/bamboo-tracker/internal/model"
 	"os"
+	"reflect"
 	"testing"
 )
 
-func TestTimeTrackingService_SendCurrentTimeTrackEmail(t *testing.T) {
+func TestTimeTrackingServiceImpl_GetCurrentTimeTracking(t *testing.T) {
 	username := "username"
 	password := "password"
-	senderEmail := "foo@bar.lol"
-	recipientEmail := "bar@foo.lol"
 
 	_ = os.Setenv("BAMBOO_USERNAME", username)
 	_ = os.Setenv("BAMBOO_PASSWORD", password)
-	_ = os.Setenv("DAILY_TIME_TRACKING_SENDER_EMAIL", senderEmail)
-	_ = os.Setenv("DAILY_TIME_TRACKING_RECIPIENT_EMAIL", recipientEmail)
 
 	anyTimeTracking := model.TimeTracking{
 		DailyTotal:  10,
 		WeeklyTotal: 20,
 	}
+
 	mockBambooApi := &mocks.MockBambooApi{}
 	mockBambooApi.GetHomeContentFunc = func() model.TimeTracking {
 		return anyTimeTracking
 	}
-	mockSesRepository := &mocks.MockSesRepository{}
 
-	timeTrackingService := InitTimeTrackingService(mockBambooApi, mockSesRepository)
+	tests := []struct {
+		name string
+		want model.TimeTracking
+	}{
+		{
+			name: "Return output of API",
+			want: anyTimeTracking,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := InitTimeTrackingService(mockBambooApi)
+			if got := s.GetCurrentTimeTracking(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetCurrentTimeTracking() = %v, want %v", got, tt.want)
+			}
+			if *mockBambooApi.LogInUsernameParam != username {
+				t.Errorf("Received unexpected login username param. Got %v", *mockBambooApi.LogInUsernameParam)
+			}
 
-	timeTrackingService.SendCurrentTimeTrackEmail()
+			if *mockBambooApi.LogInPasswordParam != password {
+				t.Errorf("Received unexpected login password param. Got %v", *mockBambooApi.LogInPasswordParam)
+			}
 
-	if mockBambooApi.LogInCalled != true {
-		t.Error("Expected login to be called")
+			if mockBambooApi.GetHomeContentCalled != true {
+				t.Error("Expected GetHomeContent to be called")
+			}
+		})
+	}
+}
+
+func TestTimeTrackingServiceImpl_UploadTimesheetEntries(t *testing.T) {
+	username := "username"
+	password := "password"
+
+	_ = os.Setenv("BAMBOO_USERNAME", username)
+	_ = os.Setenv("BAMBOO_PASSWORD", password)
+
+	var anyBambooSession = model.BambooSession{
+		EmployeeId: "123",
+		Id:         "321",
+	}
+
+	var mockBambooApi = &mocks.MockBambooApi{
+		BambooSession: anyBambooSession,
+	}
+
+	var anyTimesheetEntries = []model.TimesheetEntry{{Date: "2020-02-02", Start: "11:00", End: "12:00"}}
+
+	s := InitTimeTrackingService(mockBambooApi)
+
+	s.UploadTimesheetEntries(anyTimesheetEntries)
+
+	if !mockBambooApi.LogInCalled || !mockBambooApi.AddTimesheetRecordCalled {
+		t.Errorf("LogIn or AddTimesheet of bambooApi was not called when there are records to be added")
+	}
+
+	if !reflect.DeepEqual(anyTimesheetEntries, mockBambooApi.AddTimesheetRecordEntriesParam) {
+		t.Errorf("Bamboo API received wrong timesheetUploader params for upload")
+	}
+
+	if anyBambooSession != *mockBambooApi.AddTimesheetRecordSessionParam {
+		t.Errorf("BambooAPI received wrong session param for upload")
 	}
 
 	if *mockBambooApi.LogInUsernameParam != username {
-		t.Errorf("Received unexpected login username param. Got %v", *mockBambooApi.LogInUsernameParam)
+		t.Errorf("BambooAPI login received wrong username. Expected %v received %v", username, *mockBambooApi.LogInUsernameParam)
 	}
 
 	if *mockBambooApi.LogInPasswordParam != password {
-		t.Errorf("Received unexpected login password param. Got %v", *mockBambooApi.LogInPasswordParam)
-	}
-
-	if mockBambooApi.GetHomeContentCalled != true {
-		t.Error("Expected GetHomeContent to be called")
-	}
-
-	if mockSesRepository.SendEmailSubjectArg == "" {
-		t.Error("Received empty send email subject arg")
-	}
-
-	if mockSesRepository.SendEmailSenderArg != senderEmail {
-		t.Errorf("Received unexpected email sender address - %v", mockSesRepository.SendEmailSenderArg)
-	}
-
-	if mockSesRepository.SendEmailRecipientArg != recipientEmail {
-		t.Errorf("Received unexpected email recipuent address - %v", mockSesRepository.SendEmailRecipientArg)
-	}
-
-	if mockSesRepository.SendEmailHtmlBodyArg == "" {
-		t.Error("Received empty send email html body")
+		t.Errorf("BambooAPI login received wrong password. Expected %v received %v", password, *mockBambooApi.LogInPasswordParam)
 	}
 }
